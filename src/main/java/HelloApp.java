d1dc4e992cf69645688cad98574f6d578663d4c9
OC2427 <om.chandrakar2427@gmail.com>
2026-03-19 03:40:33 +0530

    UC4

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,11 +1,16 @@
 public class HelloApp{
 	public static void main(String[] args){
-		String name = "World";
+		String name = "World", temp_name = "";
 		if (args.length > 0) name = args[0];
 		System.out.println("Hello ");
 		for (int i = 0; i < args.length; i++){
-			if (args[i] == " ") System.out.println (",");
-			System.out.println (name);
+			if (args[i] == " ") {
+				temp_name = "";
+				System.out.println (",");
+			}
+			else {
+				temp_name = temp_name + args[i];
+			}
 		}
 		System.out.println ("!");
 	}
